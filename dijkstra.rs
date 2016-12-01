57519de862d8ba1409863e506e9cefb736655675
joshualemmon <joshua.lemmon@uoit.net>
2016-12-01 12:41:50 -0500

    program can now read in a filename as an argument

--- dijkstra.rs
+++ dijkstra.rs
@@ -1,3 +1,4 @@
+use std::env;
 
 struct Edge(String, String, i32);
 
@@ -6,14 +7,33 @@ struct Graph {
 	edges: Vec<Edge>, 
 }
 
+#[allow(non_snake_case)]
+fn generateGraph(filename: String) -> Graph {
+	let mut G = Graph{nodes: vec![],edges: vec![]};
+	G
+}
+
 
 fn main()
 {
-	let mut G = Graph{nodes: vec![],edges: vec![]};
-	G.nodes.push(String::from("hello"));
-	G.edges.push(Edge(String::from("hello"),String::from("goodbye"), 1));
-	for e in G.edges
+	#[allow(non_snake_case)]
+	let mut count = 0;
+	let mut fname = String::new();
+	for arg in env::args()
 	{
-		println!("{} -> {} with weight {}", e.0,e.1,e.2);
+		if count == 2
+		{
+			fname = arg;
+		}
+		count += 1;
 	}
+	println!("{}", fname);
+	let G = generateGraph(fname);
+	//G.nodes.push(String::from("hello"));
+	//G.edges.push(Edge(String::from("hello"),String::from("goodbye"), 1));
+	//for e in G.edges
+	//{
+	//	println!("{} -> {} with weight {}", e.0,e.1,e.2);
+	//}
+
 }
