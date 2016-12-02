fe3c457627db5482d276de4469b36138ed952a54
joshualemmon <joshua.lemmon@uoit.net>
2016-12-02 14:18:02 -0500

    Built dependencies

--- dijkstra/src/main.rs
+++ dijkstra/src/main.rs
@@ -2,16 +2,16 @@ use std::env;
 use std::fs::File;
 
 #[allow(non_snake_case)]
-fn generateGraph(filename: String) -> Graph {
+//fn generateGraph(filename: String) -> Graph {
 	//let mut G = Graph{nodes: vec![]};
 	//G
-}
+//}
 
 #[allow(non_snake_case)]
 fn main()
 {
-	let fname = env::args().nth(2).unwrap();
-	let G = generateGraph(fname);
+	//let fname = env::args().nth(2).unwrap();
+	//let G = generateGraph(fname);
 	//G.nodes.push(String::from("hello"));
 	//G.edges.push(Edge(String::from("hello"),String::from("goodbye"), 1));
 	//for e in G.edges
