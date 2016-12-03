4633f744723fe882f5f292114b9f2a23c066779d
joshualemmon <joshua.lemmon@uoit.net>
2016-12-03 12:58:38 -0500

    Fixed try! macro error when opening file

--- dijkstra/src/main.rs
+++ dijkstra/src/main.rs
@@ -1,17 +1,39 @@
+extern crate petgraph;
+
 use std::env;
 use std::fs::File;
+use petgraph::Graph;
+use std::io::BufReader;
+use std::io::BufRead;
+use std::io::Error;
 
 #[allow(non_snake_case)]
-//fn generateGraph(filename: String) -> Graph {
-	//let mut G = Graph{nodes: vec![]};
-	//G
-//}
+fn generateGraph(filename: String) -> Graph<String, i32> 
+{
+	let mut G = Graph::<String, i32>::new();
+	let mut file = match File::open(filename) 
+	{
+        Ok(file) => file,
+        Err(_) => panic!("no such file"),
+    };
+	let reader = BufReader::new(file);
+	let lines: Vec<_> = reader.lines().collect();
+	for l in lines 
+	{
+		let vals: Vec<_> = l.split("|").collect();
+		for v in vals
+		{
+			println!("{}", v);
+		} 
+	}
+	return G;
+}
 
 #[allow(non_snake_case)]
 fn main()
 {
-	//let fname = env::args().nth(2).unwrap();
-	//let G = generateGraph(fname);
+	let fname = env::args().nth(2).unwrap();
+	let G = generateGraph(fname);
 	//G.nodes.push(String::from("hello"));
 	//G.edges.push(Edge(String::from("hello"),String::from("goodbye"), 1));
 	//for e in G.edges
---
+++ dijkstra/target/debug/dijkstra.d
@@ -0,0 +1,5 @@
+/home/username/Desktop/Prog-Lang-Final/dijkstra/target/debug/dijkstra: src/main.rs
+
+/home/username/Desktop/Prog-Lang-Final/dijkstra/target/debug/dijkstra.d: src/main.rs
+
+src/main.rs:
