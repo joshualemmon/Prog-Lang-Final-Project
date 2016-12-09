6456db8f3fecf66748138fa8243efbe6c47a9eba
joshualemmon <joshua.lemmon@uoit.net>
2016-12-09 13:22:08 -0500

    Algorithm now calculates shortest paths for every node in the graph

--- dijkstra/src/main.rs
+++ dijkstra/src/main.rs
@@ -22,7 +22,7 @@ fn generate_graph<'a>(lines: Vec<&'static str>) -> UnGraphMap<&'static str, i32>
 }
 
 #[allow(non_snake_case)]
-fn dijkstra(G: UnGraphMap<&str, i32>,source: &str) -> String
+fn dijkstra(G: UnGraphMap<&str, i32>,source: &str) -> ()
 {
 	let mut dist: HashMap<&str, i32> = HashMap::new();
 	let mut parent: HashMap<&str, &str> = HashMap::new();
@@ -42,7 +42,7 @@ fn dijkstra(G: UnGraphMap<&str, i32>,source: &str) -> String
 	while swapped == true
 	{
 		swapped = false;
-		for i in 1..(n-1)
+		for i in 1..n
 		{
 			if dist[sorted[i-1]] > dist[sorted[i]]
 			{
@@ -80,10 +80,45 @@ fn dijkstra(G: UnGraphMap<&str, i32>,source: &str) -> String
 	}
 	for n in G.nodes()
 	{
-		println!("{} : {}",n,dist[n]);
+		if n != source
+		{
+			println!("From {} to {}",source,n);
+			println!("{}",printPath(source,n,&parent, &dist));
+		}
 	}
 
-	return String::new();
+	
+}
+
+fn printPath(source: &str, dest: &str, parent: &HashMap<&str, &str>, dist: &HashMap<&str, i32>) -> String
+{
+	let mut path = String::new();
+	let mut p = parent[dest];
+	path.push_str(dest);
+	path.push_str(">-");
+	while p != source
+	{
+		path.push_str(p);
+		path.push_str(">-");
+		p = parent[p];
+	}
+	path.push_str(source);
+	path = reverse(path);
+	path.push_str(" With a total cost of ");
+	path += &dist[dest].to_string();
+	path
+}
+
+fn reverse(mut s: String) -> String
+{
+	let n = s.len();
+	let mut s2 = String::new();
+	for i in 0..n
+	{
+		let c = s.pop().unwrap();
+		s2.push(c);
+	}
+	s2
 }
 
 fn def_lines<'a>() -> Vec<&'a str>
@@ -94,6 +129,7 @@ fn def_lines<'a>() -> Vec<&'a str>
 	lines.push("A|C|1");
 	lines.push("C|D|1");
 	lines.push("B|D|4");
+	lines.push("D|E|3");
 	lines
 }
 
@@ -102,6 +138,5 @@ fn main()
 {
 	let G = generate_graph(def_lines());
 	//println!("{}", G.node_count());
-	let path = dijkstra(G, "A");
-	println!("Shortest path is: {}", path);
+	dijkstra(G, "A");
 }
